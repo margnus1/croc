bb1d2429a8b9119c937dc603051716641047704d
Patrik Broman <patwotrik@Update.UU.SE>
2013-10-01 15:58:06 +0200

    main.cpp should probably not be changed much now.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "CrocGame.h"
 #include <iostream>
+#include "Crawler.cpp"
 
 int main() {
   bool noob = false;
   CrocSession s(L"Group 2", noob);
+  Crawler crawler(&s);
+  crawler.play();
   std::cout << "Hello World" << std::endl;
 }
