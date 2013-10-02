8b465b13769815a05fdc8a6395512b9e69712386
patwotrik <reg@patrikbroman.se>
2013-10-02 16:56:00 +0200

    Progress bar

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include "CrocGame.h"
 #include <iostream>
 #include "Crawler.h"
+#include "ProgressBar.h"
 
 int main() {
   bool noob = false;
   CrocSession s(L"Group 4", noob);
   const int numberOfGames = 100;
+  ProgressBar pb(numberOfGames, 80);
 
   Crawler crawler(&s);
 
   for(int i=0; i< numberOfGames; i++)
     {
       crawler.play();
-      std::cout << "Game number: " << i+1 << "  \tScore: " << crawler.finalScore << std::endl;      
+      //      std::cout << "Game number: " << i+1 << "  \tScore: " << crawler.finalScore << std::endl;      
+      pb.Tick();
     }
 
   s.PostResults();
