2c050dc55aca2df42632e8496d85decfc9883962
patwotrik <reg@patrikbroman.se>
2013-10-03 00:27:00 +0200

    Vad fan fungerar inte skiten f;r?

--- Crawler3.cpp
+++ Crawler3.cpp
@@ -7,6 +7,10 @@
 
 using namespace std;
 
+void fix(double &p) { int d = p-0.5; p+=d/10.0;};
+//void fix(double &p) { if (p<0.1) p+=0.1; p*=1/(p+0.5);};
+void pr(double p) { cout << p << endl; }
+
 void Crawler::play()
 {
   int score;
@@ -21,10 +25,12 @@ void Crawler::play()
   wstring action2;
 
   const vector<vector<int> >& paths = server->getPaths();
-  const int noNodes = paths.size();
+  //  const int noNodes = paths.size();
 
-  vector<double> probs; // probs[x] is the probability that the croc is in node x+1
-  fill (probs.begin(), probs.begin()+noNodes, 1/noNodes);
+  //  vector<double> probs; // probs[x] is the probability that the croc is in node x+1
+
+  //  for(int i=0; i<noNodes; i++)
+  // probs.push_back(0.5);
 
       server->StartGame();
       do {
@@ -32,26 +38,44 @@ void Crawler::play()
 			     backpacker1Activity, backpacker2Activity, 
 			     calcium, saline, alkalinity);
 	
-	const vector<int> localPaths = paths[playerLocation];
-	localPaths.push_back(playerLocation);
+	/*	vector<int> localPaths;
+	localPaths = paths[playerLocation-1];
+	//	localPaths.push_back(playerLocation-1);
 
-	auto maxProb = max_element(begin(localPaths), localPaths.end(), [&](int m, int n){return probs[m]<probs[n];});
+	for_each(probs.begin(), probs.end(), &fix);
 
-	action2='S';
+	auto maxProbNode = min_element(begin(localPaths), localPaths.end(), [&](int m, int n){return probs[m]<probs[n];});
+	int max = *maxProbNode;
+	//	localPaths.pop_back();
+	maxProbNode = max_element(begin(localPaths), localPaths.end(), [&](int m, int n){return probs[m]<probs[n];});
+	int secondmax = *maxProbNode;
 
-	if(*maxProb == playerLocation)
-	  action1='S';
-	else
+
+	if(backpacker1Activity<0)
+	  probs[1-backpacker1Activity]=1.0;
+	if(backpacker2Activity<0)
+	probs[1-backpacker2Activity]=1.0; */
+
+	/*	if(max == playerLocation)
 	  {
-	    localPaths.pop_back();
-	    maxProb = max_element(begin(localPaths), localPaths.end(), [&](int m, int n){return probs[m]<probs[n];});
-	    action1=ToString(*maxProb);
+	    action1='S';
+	    action2=ToString(secondmax);
 	  }
-	if(action1[0] == 'S')
-	  probs[playerLocation]=0.0;
+	  else */
+	  
+	int size = paths[playerLocation-1].size();
+	action1=ToString((long long)paths[playerLocation-1][rand()%size]-1);
+	action2='S';
+	     
+
+	/*	if(action1[0] == 'S')
+	  probs[playerLocation-1]=0.01;
 	if(action2[0] == 'S')
-	  probs[action2[0] -'0']=0.0;
-	// Exception if action1 == action2 == 'S'
-	
+	probs[action2[0] -'0'-1]=0.01;*/
+	//  Exception if action1 == action2 == 'S'
+	//	for( std::vector<double>::const_iterator i = probs.begin(); i != probs.end(); ++i)
+	//  cout << *i << ' ';
+	cout << playerLocation<< endl;
+
       } while(server->makeMove(action1, action2, finalScore));
 }
