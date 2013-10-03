a369a1064994170fba113e0d62a7d42b75110685
Magnus Lång <margnus1@telia.com>
2013-10-03 17:32:43 +0200

    Forgot files, again

---
+++ console.cpp
@@ -0,0 +1,18 @@
+#include "console.h"
+#include <iostream>
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+void Console::moveCursorUp(int lines) {
+#ifdef WIN32
+  std::cout << std::flush;
+  CONSOLE_SCREEN_BUFFER_INFO sbi;
+  HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+  GetConsoleScreenBufferInfo(console, &sbi);
+  COORD newPos = {0, sbi.dwCursorPosition.Y - lines};
+  SetConsoleCursorPosition(console, newPos);
+#else
+  std::cout << "\x1B[" << lines << "F";
+#endif
+};
---
+++ console.h
@@ -0,0 +1,6 @@
+
+class Console {
+ public:
+  // Moves the cursor 'lines' lines up, and to the beginning of the line
+  static void moveCursorUp(int lines);
+};
