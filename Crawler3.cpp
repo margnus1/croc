a583fc0466982c8b450da3c92363dd27f339ad30
patwotrik <reg@patrikbroman.se>
2013-10-03 14:18:33 +0200

    Probability calculations from step to step probably works now, but refactoring is needed.

--- Crawler3.cpp
+++ Crawler3.cpp
@@ -4,11 +4,12 @@
 #include <iostream>
 #include "ToString.h"
 #include <algorithm>
+#include "Calc.cpp"
 
 using namespace std;
 
 //void fix(double &p) { int d = p-0.5; p=p-d/10.0;};
-void fix(double &p) { if (p<0.45) p+=0.1; else if (p>0.55) p-=0.1;};
+void fix(double &p) { if (p<0.5) p+=0.1; else if (p>0.5) p-=0.1;};
 void pr(double p) { cout << p << endl; }
 
 void Crawler::play()
@@ -25,60 +26,86 @@ void Crawler::play()
   wstring action2;
 
   const vector<vector<int> >& paths = server->getPaths();
-    const int noNodes = paths.size();
+  const int noNodes = paths.size();
 
-    vector<double> probs; // probs[x] is the probability that the croc is in node x+1
+  vector<double> probs; // probs[x] is the probability that the croc is in node x+1
 
-    for(int i=0; i<noNodes; i++)
-   probs.push_back(0.5);
+  for(int i=0; i<noNodes; i++)
+    probs.push_back(1.0/noNodes);
 
-      server->StartGame();
-      do {
-	server->GetGameState(score, playerLocation, 
-			     backpacker1Activity, backpacker2Activity, 
-			     calcium, saline, alkalinity);
+  server->StartGame();
+  do {
+    server->GetGameState(score, playerLocation, 
+			 backpacker1Activity, backpacker2Activity, 
+			 calcium, saline, alkalinity);
 	
-		vector<int> localPaths;
-	localPaths = paths[playerLocation-1];
-	localPaths.push_back(playerLocation-1);
+    vector<int> localPaths;
+    localPaths = paths[playerLocation-1];
 
-	for_each(probs.begin(), probs.end(), &fix);
+    if(backpacker1Activity<0)
+      {
+	for(int i=0; i<noNodes; i++)
+	  probs[i]=0.0;
+	probs[-1-backpacker1Activity]=1.0;
+      }
+    else if(backpacker1Activity>0)
+      probs[backpacker1Activity-1]=0;
 
-	auto maxProbNode = min_element(begin(localPaths), localPaths.end(), [&](int m, int n){return probs[m]<probs[n];});
-	int max = *maxProbNode;
-	localPaths.pop_back();
-	maxProbNode = max_element(begin(localPaths), localPaths.end(), [&](int m, int n){return probs[m]<probs[n];});
-	int secondmax = *maxProbNode;
-
-
-	if(backpacker1Activity<0)
-	  probs[-1-backpacker1Activity]=1.0;
-	if(backpacker2Activity<0)
+    if(backpacker2Activity<0)
+      {
+	for(int i=0; i<noNodes; i++)
+	  probs[i]=0.0;
 	probs[-1-backpacker2Activity]=1.0; 
+      }
+    else if(backpacker2Activity>0)
+      probs[backpacker2Activity-1]=0;
+
+    ProbList old = probs;
+    calc(paths, old, probs);
+
+
+
+
+    auto maxProbNode = max_element(begin(localPaths), localPaths.end(), [&](int m, int n){return probs[m]<probs[n];});
+    int secondmax = *maxProbNode;
+    localPaths.push_back(playerLocation-1);
+    maxProbNode = max_element(begin(localPaths), localPaths.end(), [&](int m, int n){return probs[m]<probs[n];});
+    int max = *maxProbNode;
 	
-	if(max == playerLocation)
-	  {
-	    action1='S';
-	    action2=ToString(secondmax);
-	  }
-	  else 
-	    {
-	      	int size = paths[playerLocation-1].size();
-			action1=ToString((long long)localPaths[rand()%size]); 
-	      //	      action1=ToString(max);
-	action2='S';
-	    }
+    if(max == playerLocation && probs[max] >0.0)
+      {
+	action1='S';
+	action2=ToString(secondmax);
+      }
+    else 
+      {
+	int size = paths[playerLocation-1].size();
+	int newnode = (long long)localPaths[rand()%size];
+	action1 = ToString(newnode);
+	//	      action1=ToString(max);
+	//	auto mrobNode = max_element(begin(paths[max-1]), paths[max-1].end(), [&](int m, int n){return probs[m]<probs[n];});
+	//	int thirdmax = *mrobNode;
+	//			if(probs[newnode-1]>0.2)
+	if(probs[newnode]>0.0) 
+	  action2='S';
+	else{
+	  size = paths[newnode-1].size();
 
-	if(action1[0] == 'S')
-	  probs[playerLocation-1]=0.01;
-	if(action2[0] == 'S')
-	  probs[stoi(action1)-1]=0.01;
-	//  Exception if action1 == action2 == 'S'
-	//			for( int i = 0; i <probs.size(); i++)
-	//  cout << probs[i] << ' ';
-	//cout << playerLocation<< endl;
-	//	cout << backpacker1Activity << " " << backpacker2Activity << endl;
-	//		cout << max << " " << secondmax << " " << playerLocation << endl;
+	  newnode = (long long)paths[newnode-1][rand()%size];
+	  action2 = ToString(newnode);
+	}
+      }
 
-      } while(server->makeMove(action1, action2, finalScore));
+    if(action1[0] == 'S')
+      probs[playerLocation-1]=0.0;
+    if(action2[0] == 'S')
+      probs[stoi(action1)-1]=0.0;
+    //  Exception if action1 == action2 == 'S'
+    //    			for( int i = 0; i <probs.size(); i++)
+    //			  cout << setprecision(2) << setw(6) << probs[i];
+    // cout << playerLocation<< endl;
+    //		cout << backpacker1Activity << " " << backpacker2Activity << endl;
+    //			cout << max << " " << secondmax << " " << playerLocation << endl;
+
+  } while(server->makeMove(action1, action2, finalScore));
 }
