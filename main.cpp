530e2b4744734d2b6f57aed50752bd47b1b2d00f
Magnus Lång <margnus1@telia.com>
2013-10-03 22:15:13 +0200

    Better strategy, more stats

--- main.cpp
+++ main.cpp
@@ -5,43 +5,56 @@
 #include <cstring>
 #include "console.h"
 #include <iomanip>
+#include <cmath>
+#include <ctime>
 
 int main(int argc, char *argv[]) {
   bool noob = false;
   CrocSession s(L"Group 4", noob);
-  const int numberOfGames = 100;
-  //  ProgressBar pb(numberOfGames, 80);
-  double avgSum = 0.0;
+  const int minNumberOfGames = 20;
+  const int maxNumberOfGames = 400;
+  const double faithInLuck = 20.0;
+  long long total = 0;
+  long long totalSquares = 0;
+  long long count = 0;
   double bestAverage = 100000.0;
-  int count = 0;
+  time_t start = time(nullptr);
   Crawler crawler(&s);
 
-  std::cout << std::endl;
+  std::cout << std::endl << std::endl;
   do {
-    int smin = 100000, smax = 0;
-    for(int i=0; i< numberOfGames; i++)
-      {
-        crawler.play();
-        //      std::cout << "Game number: " << i+1 << "  \tScore: " << crawler.finalScore << std::endl;
-        smin = std::min(smin, crawler.finalScore);
-        smax = std::max(smax, crawler.finalScore);
-        //    pb.Tick();
+    // Play at least minNumberOfGames games, and up to
+    // maxNumberOfGames, bailing when the average gets too bad
+    for (int games = 0; games < minNumberOfGames ||
+                        (games < maxNumberOfGames &&
+                         (s.getAverage() / bestAverage) <
+                         1.0 + (faithInLuck / games));
+         games++) {
+      crawler.play();
+
+      total += crawler.finalScore;
+      totalSquares += crawler.finalScore * crawler.finalScore;
+      count++;
+
+      if (games >= 100 && s.getAverage() < bestAverage) {
+        s.PostResults();
+        bestAverage = s.getAverage();
       }
+    }
 
-
-    Console::moveCursorUp(1);
-    count++;
-    avgSum += s.getAverage();
+    double var = (double)(totalSquares - (total * total) / count) / count;
+    Console::moveCursorUp(2);
     std::cout << std::setfill(' ') << std::setiosflags(std::ios::left)
-              << "Last Avg: " << std::setw(8) << s.getAverage()
-              << "Cum Avg: "  << std::setw(8) << (avgSum / count)
-              << "Best Avg: " << std::setw(8) << bestAverage
+              << "Avg: "    << std::setw(11) << ((double)total / count)
+              << "StdDev: " << std::setw(8)  << std::sqrt(var)
+              << "Best: "   << std::setw(10) << bestAverage
               << "    " // Because Windows
               << std::endl;
-    if (s.getAverage() < bestAverage) {
-      s.PostResults();
-      bestAverage = s.getAverage();
-    }
+    std::cout << std::setfill(' ') << std::setiosflags(std::ios::left)
+              << "Games: " << std::setw(9) << count
+              << "Speed: " << std::setw(9) << count / difftime(time(nullptr), start)
+              << "    " // Because Windows
+              << std::endl;
     s.ClearRecord();
   } while (argc > 1 && strcmp(argv[1], "--forever") == 0);
 }
