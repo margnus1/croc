a13c047d5b7fc2d0762907dbde4a6e8fc7b1ab71
Rickard Lööf <loof87@hotmail.com>
2013-10-04 14:42:21 +0200

    "crawler 2, works it way towards maximum and checks proapilitys => 70%" should compile :p

--- Crawler2.cpp
+++ Crawler2.cpp
@@ -2,6 +2,24 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include "ToString.h"
+#include <algorithm>
+#include "Calc.cpp"
+
+using namespace std;
+
+namespace {
+  void reactToBackpacker(vector<double> &probs, int bpactivity) {
+    if(bpactivity<0)
+      {
+	for(int i=0; i<int(probs.size()); i++)
+	  probs[i]=0.0;
+	probs[-bpactivity-1]=1.0;
+      }
+    else if(bpactivity>0)
+      probs[bpactivity-1]=0;
+  }
+}
 
 void Crawler::play()
 {
@@ -13,39 +31,67 @@ void Crawler::play()
   double saline;
   double alkalinity;
 
-  std::wstring action1;
-  std::wstring action2;
+  wstring action1;
+  wstring action2;
 
-  int mpLocation;
+  const vector<vector<int> >& paths = server->getPaths();
+  const int noNodes = paths.size();
 
-  const std::vector<std::vector<int> >& paths = server->getPaths();
+  vector<double> probs(noNodes, 1.0/noNodes); // probs[x] is the probability that the croc is in node x+1
 
-      server->StartGame();
-      do {
-        server->GetGameState(score, playerLocation,
-                             backpacker1Activity, backpacker2Activity,
-                             calcium, saline, alkalinity);
+  server->StartGame();
+  do {
+    server->GetGameState(score, playerLocation, 
+			 backpacker1Activity, backpacker2Activity, 
+			 calcium, saline, alkalinity);
+	
+    vector<int> localPaths = paths[playerLocation-1];
 
-        mpLocation = 0; /* mplFinder()  */
-        int size=paths[playerLocation].size();
-        if(playerLocation = mpLocation)
-          {
-            action1='S'; /* we stand on the croc */
-          }
+    reactToBackpacker(probs, backpacker1Activity);
+    reactToBackpacker(probs, backpacker2Activity);
+
+    ProbList old = probs;
+    calc(paths, old, probs);
+
+    /*    auto maxProbNode = max_element(begin(localPaths), localPaths.end(),
+                                   [&](int m, int n){return probs[m-1]<probs[n-1];});
+    int secondmax = *maxProbNode;
+    localPaths.push_back(playerLocation);
+    maxProbNode = max_element(begin(localPaths), localPaths.end(),
+                              [&](int m, int n){return probs[m-1]<probs[n-1];});
+                              int max = *maxProbNode; */
+
+    int maximum = 0;
+    double temp = probs[0];
+    for (int i=0; i<probs.size(); i++) 
+      if(temp<probs[i])
+        {
+          temp = probs[i];
+          maximum = i;
+        }
+
+  if(maximum == playerLocation)
+    {
+      action1='S';
+	action2=ToString(dijkstra.next(playerLocation,maximum));
+        // step to next node towards maximum using dijkstra
+      }
+    else 
+      {
+        int next = dijkstra.next(playerLocation,maximum)
+          action1=ToString(next); // walk towards maximum
+        if (probs(next) > 0.69)
+          // if there is 70% chance that cros is here(where we walked with action1) we test search
+          action2='S';
         else
-          {
-            for(int i=0; i<size; i++ )  /* check all "grannar" efter mplocation */
-              {
-                if((paths[playerLocation][i]) = mpLocation)
-                  {
-                    action1=paths[playerLocation][i];
-                  }          
-                else
-                  {
-                    action1='S'; /* för trött för att tänka atm, fixa en bra shortest path till mpLocation */
-                  }
-                action2='S';
-              }
-          }
-      } while(server->makeMove(action1, action2, finalScore));
+          action2=ToString(dijkstra.next(next,maximum)) // walk a second time towards maximum
+	
+            }
+
+    if(action1[0] == 'S')
+      probs[playerLocation-1]=0.0;
+    if(action2[0] == 'S')
+      probs[stoi(action1)-1]=0.0;
+
+  } while(server->makeMove(action1, action2, finalScore));
 }
